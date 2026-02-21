f03f16c831c6ba9657a8d4ad4afe08689516348b
Midas <64684902+midasn74@users.noreply.github.com>
2026-02-21 11:52:18 +0100

    add extra complexity to line world

---
+++ platformer.py
@@ -0,0 +1,76 @@
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+from gymnasium.envs.registration import register
+
+class AdvancedCorridorEnv(gym.Env):
+    """
+    Custom 1D Platformer environment.
+    The agents goal is to walk right while avoiding pits, and reach the goal at the right most position.
+    """
+    def __init__(self, length=15):
+        super(AdvancedCorridorEnv, self).__init__()
+        
+        self.length = length
+        
+        # Action space -> 0: Left, 1: Right, 2: Jump Right (moves 2 spaces)
+        self.action_space = spaces.Discrete(3)
+        
+        # Observation space -> agents position integer from 0 to length-1
+        self.observation_space = spaces.Discrete(self.length)
+        
+        # Pit location
+        self.pits = [4, 8, 12]
+        
+        self.reset()
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        self.agent_pos = 0
+        self.goal_pos = self.length - 1
+        self.steps = 0
+        return self.agent_pos, {}
+
+    def step(self, action):
+        reward = -1 # Cost of walking
+        
+        if action == 0:   # Move Left
+            self.agent_pos = max(0, self.agent_pos - 1)
+        elif action == 1: # Move Right
+            self.agent_pos = min(self.length - 1, self.agent_pos + 1)
+        elif action == 2: # Jump Right
+            self.agent_pos = min(self.length - 1, self.agent_pos + 2)
+            reward = -2.5   # Cost of jumping
+            
+        self.steps += 1
+        terminated = False
+        truncated = False
+        
+        if self.agent_pos in self.pits:
+            reward = -10 # Fell in pit
+            terminated = True 
+        elif self.agent_pos == self.goal_pos:
+            reward = 10  # Reached the the goal
+            terminated = True
+            
+        # Truncate game if agents is too bad
+        if self.steps >= 5 * self.length:
+            truncated = True
+
+        return self.agent_pos, reward, terminated, truncated, {}
+
+    def render(self):
+        corridor = ["_"] * self.length
+        
+        for p in self.pits:
+            corridor[p] = "U" 
+            
+        corridor[self.goal_pos] = "G"
+        corridor[self.agent_pos] = "A" 
+        
+        print("\n" + " ".join(corridor))
+
+register(
+    id='AdvancedCorridor-v0',
+    entry_point='corridor:AdvancedCorridorEnv', 
+)
