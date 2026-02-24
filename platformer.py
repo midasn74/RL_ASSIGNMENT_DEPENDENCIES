50c7ee8ebea29fa8fd0235510c3becf61ec5b136
Midas <64684902+midasn74@users.noreply.github.com>
2026-02-24 09:19:35 +0100

    parameratize rewards

--- platformer.py
+++ platformer.py
@@ -16,10 +16,21 @@ class PlatformerEnv(gym.Env):
             2: "Jump Right"
         }
 
-    def __init__(self, length=15):
+    def __init__(self, 
+                 length=15,
+                 walk_cost=-1.0,
+                 jump_cost=-2.5,
+                 pit_penalty=-10.0,
+                 goal_reward=10.0
+    ):
         super(PlatformerEnv, self).__init__()
-        
+
         self.length = length
+
+        self.walk_cost = walk_cost
+        self.jump_cost = jump_cost
+        self.pit_penalty = pit_penalty
+        self.goal_reward = goal_reward
         
         # Action space -> 0: Left, 1: Right, 2: Jump Right (moves 2 spaces)
         self.action_space = spaces.Discrete(3)
@@ -43,24 +54,41 @@ class PlatformerEnv(gym.Env):
     @property
     def nA(self):
         return self.action_space.n
-
+        
     @property
     def P(self):
         P = {}
+
         for s in range(self.nS):
             P[s] = {}
+
             for a in range(self.nA):
-                if a == 0:
+
+                if s in self.pits or s == self.goal_pos:
+                    P[s][a] = [(1.0, s, 0.0, True, {})]
+                    continue
+
+                if a == 0:  # Left
                     next_state = max(0, s - 1)
-                    reward = -1
-                elif a == 1:
+                    reward = self.walk_cost
+                elif a == 1:  # Right
                     next_state = min(self.length - 1, s + 1)
-                    reward = -1
-                elif a == 2:
+                    reward = self.walk_cost
+                elif a == 2:  # Jump Right
                     next_state = min(self.length - 1, s + 2)
-                    reward = -2.5
-                terminated = next_state in self.pits or next_state == self.goal_pos
+                    reward = self.jump_cost
+
+                terminated = False
+
+                if next_state in self.pits:
+                    reward = self.pit_penalty
+                    terminated = True
+                elif next_state == self.goal_pos:
+                    reward = self.goal_reward
+                    terminated = True
+
                 P[s][a] = [(1.0, next_state, reward, terminated, {})]
+
         return P
 
     def reset(self, seed=None, options=None):
@@ -71,28 +99,30 @@ class PlatformerEnv(gym.Env):
         return self.agent_pos, {}
 
     def step(self, action):
-        reward = -1 # Cost of walking
-        
-        if action == 0:   # Move Left
+        if action == 0: # Left
             self.agent_pos = max(0, self.agent_pos - 1)
-        elif action == 1: # Move Right
+            reward = self.walk_cost
+
+        elif action == 1: # Right
             self.agent_pos = min(self.length - 1, self.agent_pos + 1)
+            reward = self.walk_cost
+
         elif action == 2: # Jump Right
             self.agent_pos = min(self.length - 1, self.agent_pos + 2)
-            reward = -2.5   # Cost of jumping
-            
+            reward = self.jump_cost
+
         self.steps += 1
         terminated = False
         truncated = False
-        
+
         if self.agent_pos in self.pits:
-            reward = -10 # Fell in pit
-            terminated = True 
-        elif self.agent_pos == self.goal_pos:
-            reward = 10  # Reached the the goal
+            reward = self.pit_penalty
             terminated = True
-            
-        # Truncate game if agents is too bad
+
+        elif self.agent_pos == self.goal_pos:
+            reward = self.goal_reward
+            terminated = True
+
         if self.steps >= 5 * self.length:
             truncated = True
 
